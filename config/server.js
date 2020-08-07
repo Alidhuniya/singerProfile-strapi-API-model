0e06b4d6a12d3a1142cc66fe2f5880b0275b27eb
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-06 21:22:56 -0700

    server js code changed

--- config/server.js
+++ config/server.js
@@ -1,9 +1,14 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '272a861849b9027709f9dcd3eeff55f3'),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '272a861849b9027709f9dcd3eeff55f3'),
-    },
-  },
 });
